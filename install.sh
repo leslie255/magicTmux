25a6163fc8cb503a56f645b941cfaa2cf237aa57
Peter.L <peter2005.luo@outlook.com>
2019-03-24 09:25:53 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,15 @@
-touch ~/.magicTmux-install-log.txt
-
 echo "\nRecieving objects...\n\n"
 
-cp ~/.tmux.conf ~/.tmux.previous-conf >> ~/.magicTmux-install-log.txt
-curl https://raw.githubusercontent.com/peterzililuo/magicTmux/master/tmux.conf -o ~/.tmux.conf >> ~/.magicTmux-install-log.txt
+curl https://raw.githubusercontent.com/peterzililuo/magicTmux/master/hello.txt -o ~/.hello.txt
+
+cp ~/.tmux.conf ~/.tmux.previous-conf
+curl https://raw.githubusercontent.com/peterzililuo/magicTmux/master/tmux.conf -o ~/.tmux.conf
 
 echo "\nInstalling Tmux Plugin Manager (TPM)\n\n"
 
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm >> ~/.magicTmux-install-log.txt
-
-curl https://raw.githubusercontent.com/peterzililuo/magicTmux/master/hello.txt -o ~/.hello.txt >> ~/.magicTmux-install-log.txt
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 echo "\n\n\n"
 cat ~/.hello.txt
+rm -rf ~/.hello.txt
 echo "\nInstallation Finished\nrestart your tmux & do <C-x> I to see effects\n\n"
