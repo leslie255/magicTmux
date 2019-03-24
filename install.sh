bfc2e905e32a90df087da4de3d2bc561f1908b62
Peter.L <peter2005.luo@outlook.com>
2019-03-24 09:15:27 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,16 @@
+touch ~/.magicTmux-install-log.txt
+
 echo "\nRecieving objects...\n\n"
 
-cp ~/.tmux.conf ~/.tmux.previous-conf
-curl https://raw.githubusercontent.com/peterzililuo/magicTmux/master/tmux.conf -o ~/.tmux.conf
+cp ~/.tmux.conf ~/.tmux.previous-conf >> ~/.magicTmux-install-log.txt
+curl https://raw.githubusercontent.com/peterzililuo/magicTmux/master/tmux.conf -o ~/.tmux.conf >> ~/.magicTmux-install-log.txt
 
 echo "\nInstalling Tmux Plugin Manager (TPM)\n\n"
 
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm >> ~/.magicTmux-install-log.txt
 
-curl https://raw.githubusercontent.com/peterzililuo/magicTmux/master/hello.txt -o ~/.hello.txt
+curl https://raw.githubusercontent.com/peterzililuo/magicTmux/master/hello.txt -o ~/.hello.txt >> ~/.magicTmux-install-log.txt
 
 echo "\n\n\n"
 cat ~/.hello.txt
-echo "\nInstallation Finished\npls do <prefix> + I to install plugins & restart your tmux to see effects\n\n"
+echo "\nInstallation Finished\nrestart your tmux & do <C-x> I to see effects\n\n"
