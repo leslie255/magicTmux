0a53a8a1b3e7e567b79977d72453e5a44ca5fe33
Peter Luo <zl2005.luo@icloud.com>
2020-07-10 19:15:02 +0800

    Add backup feature to install process

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 
+# magicTmux message
 echo "\033[31m                           _       "
 echo "                          (_)      "
 echo "  _ __ ___    __ _   __ _  _   ___ "
@@ -15,36 +16,65 @@ echo "    | || '_ \` _ \\ | | | |\\ \\/ /    "
 echo "    | || | | | | || |_| | >  <     "
 echo "    |_||_| |_| |_| \\__,_|/_/\\_\\    "
 
-rm -rf ~/.tmux
+# Make sure tmux is installed
+if ! command -v tmux &> /dev/null
+then
+echo "\033[0m Tmux is not installed, "
+exit 1
+fi
+
+dialog() {
+   echo "\033[32m$1\033[34m"
+}
+
+clone() {
+    dialog "\nInstalling $1..."
+    git clone $2 $3
+}
+
+mkdirIfNeeded() {
+    if [ ! -d $1 ]; then
+        mkdir $1
+    fi
+}
 
 echo "\033[33m\n... Is now installing"
 
+# Backup existing tmux configs (if needed)
+bakupDir=~/.tmux-config-bakups
+bakupDate=$(date +"%Y-%m-%d(%T)")
+if [ -d ~/.tmux/ ]; then
+    dialog "backing up ~/.tmux to ${backupDir}/${bakupDate}/tmux..."
+    mkdirIfNeeded $bakupDir
+    mkdirIfNeeded $bakupDir/$bakupDate/
+    mv ~/.tmux $bakupDir/$bakupDate/tmux/
+fi
+if [ -f ~/.tmux.conf ]; then
+    dialog "backing up ~/.tmux.conf to ${bakupDir}${bakupDate}/tmux.conf..."
+    mkdirIfNeeded $bakupDir
+    mkdirIfNeeded $bakupDir/$bakupDate/
+    mv ~/.tmux.conf $bakupDir/$bakupDate/tmux.conf
+fi
+
+# download files...
+dialog "\nfetching tmux.conf..."
 touch ~/.tmux.conf
 curl -s https://raw.githubusercontent.com/p-z-l/magicTmux/master/tmux.conf -o ~/.tmux.conf
+clone "Tmux Plugin Manager (TPM)" https://github.com/tmux-plugins/tpm.git ~/.tmux/plugins/tpm
+clone "tmux-battery" https://github.com/tmux-plugins/tmux-battery.git ~/.tmux/plugins/tmux-battery
+clone "tmux-cpu" https://github.com/tmux-plugins/tmux-cpu.git ~/.tmux/plugins/tmux-cpu
+clone "tmux-prefix-highlight" https://github.com/tmux-plugins/tmux-prefix-highlight.git ~/.tmux/plugins/tmux-prefix-highlight
 
-echo "\033[32m\nInstalling Tmux Plugin Manager (TPM)\033[34m"
-
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-echo "\033[32m\n\nInstalling tmux-battery\033[34m"
-
-git clone https://github.com/tmux-plugins/tmux-battery.git ~/.tmux/plugins/tmux-battery
-
-echo "\033[32m\n\nInstalling tmux-cpu\033[34m"
-
-git clone https://github.com/tmux-plugins/tmux-cpu.git ~/.tmux/plugins/tmux-cpu
-
-echo "\033[32m\n\nInstalling tmux-prefix-highlight\033[34m"
-
-git clone https://github.com/tmux-plugins/tmux-prefix-highlight.git ~/.tmux/plugins/tmux-prefix-highlight
-
+# reload tmux config
 tmux source-file ~/.tmux.conf
 
-echo "\033[32m\nInstallation Finished"
+dialog "magicTmux installation Finished"
 
-if [ -f $TMUX ]; then
+# tell user to launch tmux if needed
+if -f $TMUX
+then
 	echo "Use command \"tmux\" to launch tmux"
 fi
 
-echo "\033[34mfor more info, see: \033[0mhttps://github.com/p-z-l/magicTmux"
+echo "\033[34mMore about magicTmux, visit: \033[0mhttps://github.com/p-z-l/magicTmux"
 echo "\n\033[0m"
